9bdefac707e77847a581a55ade65ac009a4e16a3
AnakTentara <anaktentara25@gmail.com>
2026-04-11 21:18:45 +0700

    update final

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [{
-        name: 'index',
+        name: 'haikaru-bot',
         script: 'index.js',
         watch: true,
-        ignore_watch: ['data', 'config', '*.log']
+        ignore_watch: ['data', 'config', '*.log', 'baileys_auth_info', 'baileys_auth_info_saran', 'memory.json']
     }]
 };
